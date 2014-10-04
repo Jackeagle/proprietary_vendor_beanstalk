42d5ffc01a553cbc1b5d1f670f59bdf6a23fb260
Shaun <shaunsibu@gmail.com>
2014-10-03 20:12:07 -0600

    Lets build our own Xposed Apps
    Change-Id: I0fc7d93fcced09d8b08edcf1a88a093e5c41f310

--- config/common.mk
+++ config/common.mk
@@ -148,9 +148,7 @@ PRODUCT_COPY_FILES +=  \
     vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so \
     vendor/cm/prebuilt/hololauncherhd/HoloLauncherHD.apk:system/app/HoloLauncherHD.apk \
     vendor/cm/prebuilt/Nova.apk:system/app/Nova.apk \
-    vendor/cm/prebuilt/appsetting.apk:system/app/appsetting.apk \
-    vendor/cm/prebuilt/xposed_installer.apk:system/app/xposed_installer.apk
-
+ 
 # Bring in camera effects
 PRODUCT_COPY_FILES +=  \
     vendor/cm/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
@@ -208,7 +206,9 @@ PRODUCT_PACKAGES += \
     LockClock \
     DashClock \
     CMHome \
-    StalkExplorer
+    StalkExplorer \
+    XposedAppSettings \
+    XposedInstaller
 
 PRODUCT_PACKAGES += \
     CellBroadcastReceiver
